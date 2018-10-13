524470d0bbd3985ff73bec12f6d8a7cecb365968
Max Morehead <moreheadmax@gmail.com>
2018-10-12 22:05:05 -0700

    Change name of addon and change window to addEventListener

--- firefox/borderify.js
+++
@@ -1,8 +0,0 @@
-window.onkeydown = function(e) {
-    var key = e.keyCode ? e.keyCode : e.which;
-
-    if (key == 70 && e.ctrlKey) {
-        e.preventDefault();
-        console.log("hi")
-    }
-}
