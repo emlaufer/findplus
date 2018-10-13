df6875eb1ef3ddb4a04fa018aa286e161258a6aa
Evan Laufer <evan.m.laufer@gmail.com>
2018-10-12 23:55:10 -0700

    added search bar

--- firefox/findplus.js
+++ firefox/findplus.js
@@ -3,8 +3,9 @@ window.addEventListener("keydown", function(e) {
 
     if (key == 70 && e.ctrlKey || key == 70 && e.metaKey) {
         e.preventDefault();
-        console.log("hi")
     }
 });
 
-//$("body").append();
+url = browser.extension.getURL("searchbar.html");
+frame = $("body").append("<iframe src=" + url + " style='position: fixed; bottom: 0; left: 0; z-index: 2139999999; width: 100%; height: 100vh'></iframe>");
+
