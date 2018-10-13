03446751edd85f80ac2725596353a920ae98b62e
Evan Laufer <evan.m.laufer@gmail.com>
2018-10-13 11:02:43 -0700

    fixed ctrl-f issue in iframe and refactored css into seperate file

--- firefox/findplus.js
+++ firefox/findplus.js
@@ -1,11 +1,11 @@
 window.addEventListener("keydown", function(e) {
     var key = e.keyCode ? e.keyCode : e.which;
 
-    if (key == 70 && e.ctrlKey || key == 70 && e.metaKey) {
+    if (key == 70 && (e.ctrlKey || e.metaKey)) {
         e.preventDefault();
     }
 });
 
 url = browser.extension.getURL("searchbar.html");
-frame = $("body").append("<iframe src=" + url + " style='position: fixed; bottom: 0; left: 0; z-index: 2139999999; width: 100%; height: 100vh'></iframe>");
+frame = $("body").append("<iframe src=" + url + " style='position: fixed; bottom: 0; left: 0; z-index: 2139999999; width: 100%; height: 3em' frameborder=0></iframe>");
 
---
+++ firefox/searchbar.js
@@ -0,0 +1,7 @@
+window.addEventListener("keydown", function(e) {
+    var key = e.keyCode ? e.keyCode : e.which;
+
+    if (key == 70 && (e.ctrlKey || e.metaKey)) {
+        e.preventDefault();
+    }
+});
