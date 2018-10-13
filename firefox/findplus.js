5e66b6ecf1a89bea88bebe991f718cac16b0b8bd
Arvind Saripalli <mail2asrs@gmail.com>
2018-10-12 23:47:37 -0700

    Added text extraction in webpage.

--- firefox/findplus.js
+++ firefox/findplus.js
@@ -1,9 +1,26 @@
+// Get all visible text in webpage.
+var doc = ""
+document.querySelectorAll('body *').forEach(function(node) {
+    if(node.localName != "script" && node.localName != "style") {
+        doc += extractText(node).trim();
+    }
+});
+
+// Return all visible text given an element.
+function extractText(element) {
+    var text;
+    text = $(element).clone().find("script,style").remove().end().text();
+    return text;
+}
+
+// Ctrl-F event listener.
 window.addEventListener("keydown", function(e) {
     var key = e.keyCode ? e.keyCode : e.which;
 
     if (key == 70 && e.ctrlKey || key == 70 && e.metaKey) {
         e.preventDefault();
-        console.log("hi")
+        console.log(doc);
+            
     }
 });
 
