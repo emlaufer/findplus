018a49c10bc515c58cdfa2b733043400dc5bef32
Evan Laufer <evan.m.laufer@gmail.com>
2018-10-12 21:55:03 -0700

    refactor keycode check

--- firefox/borderify.js
+++ firefox/borderify.js
@@ -1,7 +1,5 @@
 window.onkeydown = function(e) {
-    var key = e.keyCode ? e.keyCode : e.which;
-
-    if (key == 70 && e.ctrlKey) {
+    if (e.keyCode == 70 && e.ctrlKey) {
         e.preventDefault();
         console.log("hi")
     }
