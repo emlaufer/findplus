059ce2d8fc3f48c85fe4f1f94d9a808f3f646863
Evan Laufer <evan.m.laufer@gmail.com>
2018-10-12 22:54:46 -0700

    mac support?

--- firefox/findplus.js
+++ firefox/findplus.js
@@ -1,10 +1,10 @@
 window.addEventListener("keydown", function(e) {
     var key = e.keyCode ? e.keyCode : e.which;
 
-    if (key == 70 && e.ctrlKey) {
+    if (key == 70 && e.ctrlKey || key == 70 && e.metaKey) {
         e.preventDefault();
         console.log("hi")
     }
 });
 
-//$("body").append("text");
+//$("body").append();
